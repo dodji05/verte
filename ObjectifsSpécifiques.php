62b0783533e7cec91916363196ff505c50ed7929
dodji05 <gildas31@gmail.com>
2018-11-14 06:36:22 +0100

    qui sommes nous

---
+++ ObjectifsSpécifiques.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="Description" content="AC3A Association des Chambres d'Agriculture de l'Arc Atlantique" />
+    <meta name="Keywords" content="ac3a, AC3A, arc atlantique, formation, consultant, programme européen, programmes européens, europeen, europe, projet européen, projets européens, avec, equal, stratega, agate, interreg, fonds européens, agriculture, région, régions, territoire, territoires, rural, ruraux,économie, sociale, formation, environnement  " />
+    <title>AC3A - Qui sommes nous ?</title>
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+    <style type="text/css">
+        <!--
+        body {
+            margin: 0px 0px 0px 0px;
+        }
+        -->
+    </style>
+</head>
+<body>
+<div align="center">
+    <div class="general">
+        <div id="bandeau"><div class="retour_accueil"><a href="AC3A.php"><img src="images/transparent_logo.png" width="180" height="65" /></a></div>
+            <div class="versionEN"><a href="http://www.ac3a.fr/AC3A_en.php"><img src="images/drapeaux/drapeau_versionEN.png" width="800" height="22" /></a></a></div>
+            <div id="bloc_menu_bandeau">
+
+                <div class="bouton_contact"><a href="contact.php?mb=1">contact</a></div>
+                <div class="bouton_menu_haut_actif"><a href="qui_sommes_nous.php?m=2&p=bref_historique&mb=2">Qui sommes nous ?</a></div>
+                <div class="bouton_menu_haut"><a href="action_politique.php?m=6&p=agricultures_atlantiques&mb=6">Action politique</a></div>
+                <div class="bouton_menu_haut"><a href="missions_projets.php?m=3&p=mission_ac3a&mb=3">NOS missions et projets</a></div>
+                <div class="bouton_menu_haut"><a href="https://ac3anews.wordpress.com/" target="_blank" >actualités - vie du réseau</a></div>
+                <div class="bouton_menu_haut"><a href="ressources.php?p=economie_de_proximite&mb=5">centre de ressources</a></div>
+
+            </div>
+        </div>
+    </div>
+
+    <div align="center">
+        <div id="general_projets">
+            <div id="page_entete">qui sommes nous ?</div>
+            <div id="menu_gauche">
+
+                <div class="menu_gauche1_2">
+
+                    <div class="menu_actif2"><a href="qui_sommes_nous.php?m=2&p=bref_historique&mb=2">Notre objectif général </a></div>
+                    <div class="menu_gauche2"><a href="qui_sommes_nous.php?m=2&p=organisation_ac3a&mb=2">Nos Objectifs Spécifiques</a></div>
+                    <div class="menu_gauche2"><a href="qui_sommes_nous.php?m=2&p=organigramme_ac3a&mb=2">Nos missions</a></div>
+                    <div class="menu_gauche2"><a href="qui_sommes_nous.php?m=2&p=mission_politique&mb=2">Nos valeurs </a></div>
+                    <div class="menu_gauche2"><a href="qui_sommes_nous.php?m=2&p=organisation_inter_Chambres&mb=2">Moyens d'action</a></div>
+
+
+
+<!--                    <div class="menu_gauche2"><a href="qui_sommes_nous.php?m=2&p=chambres_ac3a&mb=2">Les Chambres de l'AC3A</a></div>-->
+
+                    <!--<div class="menu_2_footer"></div>-->
+
+                    <!--<div class="menu_1_footer"></div>-->
+                </div>
+
+            </div>
+            <div id="page">
+                <link href="ac3aCSS3.css" rel="stylesheet" type="text/css" />
+                <div id="langues"></div>
+
+                <div class="page_contenu" id="general">
+                    <h3>Objectif général</h3>
+                    <br/>
+                    <p>
+
+                        MAIN VERTE a pour objectif général de contribuer à la conservation, la protection et la gestion durable de la biodiversité dans un souci d’amélioration du cadre de vie.
+                    </p>
+                    <br/>
+                    <br/>
+                </div>
+
+                <div class="page_contenu" id="specfi">
+                    <h3>Objectifs Spécifiques</h3>
+                    <br/>
+                    <ul>
+                        <li>
+                            Promouvoir les bonnes pratiques de protection, conservation et gestion durable de la biodiversité
+                        </li>
+                        <li>
+                            Promouvoir des mesures de résilience aux changements climatiques dans l’agriculture
+                        </li>
+                        <li>
+                            Valoriser les contributions des agriculteurs dans la sécurité alimentaire
+                        </li>
+                        <li>
+                            Promouvoir les droits des agriculteurs.
+                        </li>
+                    </ul>
+                    <br/>
+                    <br/>
+                </div>
+
+                <div class="page_contenu" id="missions">
+                    <h3>Nos missions</h3>
+                    <br/>
+                    <ul>
+                        <li>
+                            Relever les défis d’intérêt général de conservation, de protection et de gestion durable de la biodiversité
+                        </li>
+                        <li>
+                            S'impliquer activement au débat environnemental,
+                        </li>
+                        <li>
+                            Mener des actions pour la préservation du patrimoine naturel et la qualité du cadre de vie.
+                        </li>
+                        <li>
+                            Militer pour une agriculture économiquement efficace
+                        </li>
+                        <li>
+                            Travailler à la reconnaissance des contributions des agriculteurs ainsi que de leurs droits.
+                        </li>
+
+                    </ul>
+
+                    <br/>
+                    <br/>
+                </div>
+
+                <div class="page_contenu" id="valeurs">
+                    <h3>nos valeurs</h3>
+                    <br/>
+                    <ul>
+                        <li>
+                            Le respect de l’environnement
+                        </li>
+                        <li>
+                            L’intérêt général, l’humanisme et la solidarité
+                        </li>
+                        <li>
+                            La connaissance et le professionnalisme
+                        </li>
+                        <li>
+                            Le plaisir et le partage...
+                        </li>
+                    </ul>
+                    <br/>
+                    <br/>
+                </div>
+
+                <div class="page_contenu" id="actions">
+                    <h3>Moyens d'action</h3>
+                    <br/>
+                    <ul>
+                        <li>
+                            Mobilisation dans une démarche synergique avec les pouvoirs publics, les partenaires au développement, les conventions internationales, les acteurs économiques, techniques et sociaux qui s’impliquent dans le développement durable et l’agriculture.
+                        </li>
+                        <li>
+                            Formations et études
+                        </li>
+                        <li>
+                            Renforcement de capacités des communautés à la base
+                        </li>
+                        <li>
+                            Campagnes et mobilisations sociales
+                        </li>
+                        <li>
+                            Actions sociales et solidaires
+                        </li>
+                        <li>
+                            Plaidoyer et lobbying
+                        </li>
+                        <li>
+                            Partenariat, partage et négociations
+                        </li>
+
+                        <li>
+                            Productions des émissions TV et Radio
+                        </li>
+                        <li>
+                            Production et diffusion de Magazines
+
+                        </li>
+                    </ul>
+                    <br/>
+                    <br/>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    <div align="center">
+        <div class="general">
+
+            <div id="footer"><img src="images/separation_footer.gif" />AC3A – Maison de l'agriculture - Rue Pierre-Adolphe Bobierre - La Géraudière – 44939 Nantes Cedex 9<br />
+                Tél. : 02 53 46 60 55
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
