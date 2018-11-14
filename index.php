5e70be513d5f6002e3781376ac32f10aa5603aaf
dodji05 <gildas31@gmail.com>
2018-11-14 06:27:23 +0100

    initail

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="Description" content="ONG LA MAIN VERTE"/>
+    <meta name="Keywords"
+          content=""/>
+    <title>ONG main verte - un cadre de vie sain pour un meilleur monde</title>
+
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
+    <style type="text/css">
+        <!--
+        body {
+            margin-left: 0px;
+            margin-top: 0px;
+            margin-right: 0px;
+            margin-bottom: 0px;
+        }
+
+        -->
+    </style>
+</head>
+<body>
+
+<div align="center">
+    <div class="general">
+
+        <div id="bandeau">
+            <div class="retour_accueil"><a href="#"><img src="images/transparent_logo.png" width="180"
+                                                                          height="65"/></a></div>
+
+            <div id="bloc_menu_bandeau">
+ 
+                <div class="bouton_contact"><a href="#">Accueil</a></div>
+                <div class="bouton_menu_haut"><a href="mainverte.php">Qui sommes nous
+                        ?</a></div>
+                <div class="bouton_menu_haut"><a href="#"> Nos projets</a></div>
+                <div class="bouton_menu_haut"><a href="missions_projets.php?m=3&p=mission_ac3a&mb=3"> Grenier d'Afrique</a></div>
+                <div class="bouton_menu_haut"><a href="https://ac3anews.wordpress.com/" target="_blank">Centre de ressources</a></div>
+                <div class="bouton_menu_haut"><a href="ressources.php?p=economie_de_proximite&mb=5">Contact</a></div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<div align="center">
+    <div class="general">
+        <div id="page_entete">Main verte</div>
+        <div class="page_contact">
+            <div class="blocs_contacts">
+                <div class="bloc_contact">
+                    <h1>COORDONNéES</h1>
+                    <h2>ONG MAIN VERTE</h2>
+                    <div class="bloc_contact_texte"><br/>
+                        <p class="nom_contact">Chef de projet : Pascal DAGRON<br/>Chargée de projets : Sarah BEIGEL
+                        </p><br/>
+                        <p>Fidjrosse<br/>
+                            - <br/>
+                            - <br/><br/>
+                            Tél : +<br/>
+                            <br/>
+                            Mail : <a href="#">contact Main verte</a></p><br/>
+                    </div>
+                </div>
+
+                <div class="bloc_contact" style="background-color:#FFFFFF; font-size:12px; text-align:left;">
+                    <h3 style="text-align:center; padding:10px;">ONG<br/> Main verte</h3>
+                    <div style="margin:10px;">
+                        <p>
+                            MAIN VERTE a pour objectif général de contribuer à la conservation, la protection et la
+                            gestion durable de la biodiversité dans un souci d’amélioration du cadre de vie.
+                        </p>
+                        <p>&nbsp;</p>
+                        <p>
+                            Ses objectifs specifiques sont :
+                        <ul>
+                            <li>
+                                Promouvoir les bonnes pratiques de protection, conservation et gestion durable de la biodiversité
+                            </li>
+                            <li>
+                                Promouvoir des mesures de résilience aux changements climatiques dans l’agriculture
+                            </li>
+                            <li>
+                                Valoriser les contributions des agriculteurs dans la sécurité alimentaire
+                            </li>
+                            <li>
+                                Promouvoir les droits des agriculteurs.
+                            </li>
+                        </ul>
+                        </p>
+                        <p>&nbsp;</p>
+
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+        <div id="page">
+            <div class="page_contenu">
+                <p>&nbsp;</p>
+                <p style="text-align:center;"><a href="https://ac3anews.wordpress.com/" target="_blank"><img
+                                src="images/ac3a/Mosaique.jpg" width="580" height="579" border="0"/></a>
+                </p>
+                <p>&nbsp;</p>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+</div>
+<div align="center">
+    <div class="general">
+        <div id="footer"><img src="images/separation_footer.gif"/>AC3A – Maison de l'agriculture - Rue Pierre-Adolphe
+            Bobierre - La Géraudière – 44939 Nantes Cedex 9<br/>
+            Tél. : 02 53 46 60 55
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ structure.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="Description" content="AC3A Association des Chambres d'Agriculture de l'Arc Atlantique" />
+    <meta name="Keywords" content="ac3a, AC3A, arc atlantique, formation, consultant, programme européen, programmes européens, europeen, europe, projet européen, projets européens, avec, equal, stratega, agate, interreg, fonds européens, agriculture, région, régions, territoire, territoires, rural, ruraux,économie, sociale, formation, environnement  " />
+    <title>AC3A - Qui sommes nous ?</title>
+    <link href="ac3aCSS3.css" rel="stylesheet" type="text/css" />
+    <style type="text/css">
+        <!--
+        body {
+            margin: 0px 0px 0px 0px;
+        }
+        -->
+    </style>
+</head>
+<body>
+<div align="center">
+    <div class="general">
+        <div id="bandeau"><div class="retour_accueil"><a href="AC3A.php"><img src="images/transparent_logo.png" width="180" height="65" /></a></div>
+            <div class="versionEN"><a href="http://www.ac3a.fr/AC3A_en.php"><img src="images/drapeaux/drapeau_versionEN.png" width="800" height="22" /></a></a></div>
+            <div id="bloc_menu_bandeau">
+
+                <div class="bouton_contact"><a href="contact.php?mb=1">contact</a></div>
+                <div class="bouton_menu_haut_actif"><a href="qui_sommes_nous.php?m=2&p=bref_historique&mb=2">Qui sommes nous ?</a></div>
+                <div class="bouton_menu_haut"><a href="action_politique.php?m=6&p=agricultures_atlantiques&mb=6">Action politique</a></div>
+                <div class="bouton_menu_haut"><a href="missions_projets.php?m=3&p=mission_ac3a&mb=3">NOS missions et projets</a></div>
+                <div class="bouton_menu_haut"><a href="https://ac3anews.wordpress.com/" target="_blank" >actualités - vie du réseau</a></div>
+                <div class="bouton_menu_haut"><a href="ressources.php?p=economie_de_proximite&mb=5">centre de ressources</a></div>
+
+            </div>
+        </div>
+    </div>
+
+    <div align="center">
+        <div id="general_projets">
+            <div id="page_entete">qui sommes nous ?</div>
+            <div id="menu_gauche">
+
+                <div class="menu_gauche1_2">
+
+                    <div class="menu_actif2"><a href="qui_sommes_nous.php?m=2&p=bref_historique&mb=2">Bref historique</a></div>
+                    <div class="menu_gauche2"><a href="qui_sommes_nous.php?m=2&p=organisation_ac3a&mb=2">Organisation de l’AC3A</a></div>
+                    <div class="menu_gauche2"><a href="qui_sommes_nous.php?m=2&p=organigramme_ac3a&mb=2">Organigramme de l’AC3A</a></div>
+                    <div class="menu_gauche2"><a href="qui_sommes_nous.php?m=2&p=mission_politique&mb=2">Une mission politique</a></div>
+                    <div class="menu_gauche2"><a href="qui_sommes_nous.php?m=2&p=organisation_inter_Chambres&mb=2">L’AC3A : Une organisation inter Chambres</a></div>
+
+
+
+                    <div class="menu_gauche2"><a href="qui_sommes_nous.php?m=2&p=chambres_ac3a&mb=2">Les Chambres de l'AC3A</a></div>
+
+                    <!--<div class="menu_2_footer"></div>-->
+
+                    <!--<div class="menu_1_footer"></div>-->
+                </div>
+
+            </div>
+            <div id="page">
+                <link href="ac3aCSS3.css" rel="stylesheet" type="text/css" />
+                <div id="langues"></div>
+
+                <div class="page_contenu">
+                    <h3>Bref historique</h3>
+                    <br/>
+                    <p><b>1973</b> : création de la CRPM (Conférence des Régions Périphériques et Maritimes)</p>
+                    <p>&nbsp;</p>
+                    <p><b>1990</b> : création de la Commission Arc Atlantique : faire face aux handicaps des régions  Atlantiques (banane bleue).<br/>
+                        Cependant, le secteur de l’agriculture n’était pas traité par la Commission Arc Atlantique<br/>
+                        Au début des années 90, les 5 chambres régionales d’agriculture et les 23 Chambres départementales décident de travailler en commun et d’accroître leurs échanges pour élaborer de nouvelles approches et dégager de nouvelles missions répondant aux changements qui affectent le monde rural et agricole.</p>
+                    <p>&nbsp;</p>
+                    <p><b>1993</b> : Création de l’AC3A (Association des Chambres d’agriculture de l’Arc Atlantique)</p>
+                    <p>&nbsp;</p>
+                    <p><b>1995</b> : Dépôt des statuts officiels – Association loi 1901</p>
+                    <br/>
+                    <br/>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    <div align="center">
+        <div class="general">
+
+            <div id="footer"><img src="images/separation_footer.gif" />AC3A – Maison de l'agriculture - Rue Pierre-Adolphe Bobierre - La Géraudière – 44939 Nantes Cedex 9<br />
+                Tél. : 02 53 46 60 55
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
